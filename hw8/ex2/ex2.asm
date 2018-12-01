6ecb1bb5cf6c2f519652901ea92cb8f8bfbd4474
AvramPop <dany4god@gmail.com>
2018-12-01 16:51:21 +0200

    final hw8

--- hw8/ex2/ex2.asm
+++ hw8/ex2/ex2.asm
@@ -1,3 +1,5 @@
+;27. A text file is given. The text file contains numbers (in base 10) separated by spaces. Read the content of the file, determine the minimum number (from the numbers that have been read) and write the result at the end of file.
+
 bits 32 
 
 global start        
@@ -13,18 +15,21 @@ import fwrite msvcrt.dll
 
 ; our data is declared here 
 segment data use32 class=data
-file_name db "input.txt", 0   ; 
+file_name db "input.txt", 0   
     access_mode db "a", 0       
     file_descriptor dd -1      
 min dd 0
     nume_fisier db "input.txt", 0 
+nume_fisier_app db "input.txt", 0    
     mod_acces db "r", 0             
+    appendMod db "a", 0
     descriptor_fis dd -1     
+descriptor_fis_app dd -1     
     nr_car_citite dd 0              
     len equ 1        
-    one db 1    ;
+one db 1    ;
     ten db 10
-    hundred db 100
+        hundred db 100
     formatmin db "min = %d ", 0
     format db "n = %d ", 0
     newline db " min = ", 0
@@ -35,23 +40,23 @@ min dd 0
 ; our code starts here
 segment code use32 class=code
     start:
-        ; open file reading mode
+                ; open file reading mode
         push dword mod_acces
         push dword nume_fisier
         call [fopen]
         add esp, 4*2
-        ; if error jump to end
+ ; if error jump to end
         cmp eax, 0                  
         je final
         
         mov [descriptor_fis], eax  
-        mov ebx, 100000 ; ebx will hold the minimum
-        mov edx, 0 ; edx will be the actual number read
+        mov ebx, 100000; ebx will hold the minimum
+        mov edx, 0; edx will be the actual number read
         bucla:
 
             push edx ; save edx
             ; read first char in file
-            push dword [descriptor_fis] 
+            push dword [descriptor_fis]
             push dword len
             push dword 1
             push dword buffer
@@ -59,32 +64,41 @@ segment code use32 class=code
             add esp, 4*4
             
             pop edx ; retrieve edx
-            cmp eax, 0  ; if final of file jump to end of loop        
+            cmp eax, 0   ; if final of file jump to end of loop          
             je end_loop
 
             mov [nr_car_citite], eax      
-            ; save into eax the char read
+             ; save into eax the char read
             mov esi, buffer
             mov eax, 0 
             lodsb
-            cmp eax, ' ' 
-            ; if eax is space, compare the actual number to minimum, else buil the actual number
+            cmp eax, ' '
+            ;if eax is space, compare the actual number to minimum, else buil the actual number
             je compare_to_min
+            ; check if actual number  (in edx) is smaller than minimum (ebx). if true, assign_min
             jne build_actual_number
                 
                 compare_to_min:
-                ; check if actual number  (in edx) is smaller than minimum (ebx). if true, assign_min
+                push edx ; save edx
+            
+                push dword edx
+                push dword format 
+                call [printf]     
+                add esp, 4 * 2 
+                
+                pop edx ; retrieve edx
+                
                 cmp ebx, edx 
                 
                 ja assign_min
                 mov edx, 0
                 jmp bucla
-                
+                 ; append last digit read to actual number in edx
                     assign_min:
                     mov ebx, edx
                     mov edx, 0
                     jmp bucla
-                ; append last digit read to actual number in edx
+            
                 build_actual_number:
                     sub eax, '0' ; eax = last digit read
                     mov ecx, eax ; save last digit read in ecx
@@ -98,7 +112,15 @@ segment code use32 class=code
             
         
       end_loop:
-            ; if last number smaller than minim, assign
+            push edx ; save edx
+            
+            push dword edx ;ebx holds min, edx holds actual number
+            push dword format 
+            call [printf]      
+            add esp, 4 * 2 
+            
+            pop edx ; retrieve edx
+            
             cmp ebx, edx ;ebx holds min, edx holds actual number
                 
             ja assign_min1
@@ -107,14 +129,18 @@ segment code use32 class=code
                 assign_min1:
                 mov ebx, edx
                 jmp cleanup
-                
         cleanup:
+         ; close file reading mode
         
-        ; close file reading mode
         push dword [descriptor_fis]
         call [fclose]
         add esp, 4
-; open file append mode
+        ; open file append mode
+                push dword ebx
+            push dword formatmin 
+            call [printf]      
+            add esp, 4 * 2 
+
         push dword access_mode     
         push dword file_name
         call [fopen]
@@ -124,19 +150,18 @@ segment code use32 class=code
         je final
 ;print format for minimum in file
         push dword newline
-        push dword [file_descriptor]
-        call [fprintf]
-        add esp, 4*2
-        pop eax
+                push dword [file_descriptor]
+                call [fprintf]
+                add esp, 4*2
+                pop eax
         ;save minimum (ebx) into eax
         mov eax, ebx
         div byte [hundred]
-        ;get hundreds digit of minimum. if existent, print to file
-        mov bh, ah
+         ;get hundreds digit of minimum. if existent, print to file
+                        mov bh, ah
         cmp al, 0
         jne print_digit_in_al1
         jmp go_on1
-        
             print_digit_in_al1:
          
                 add al, '0'
@@ -154,12 +179,11 @@ segment code use32 class=code
         mov al, bh
         mov ah, 0
         div byte [ten]
-        ;get tens digit of minimum. if existent, print to file
+          ;get tens digit of minimum. if existent, print to file
         mov bh, ah
         cmp al, 0
         jne print_digit_in_al2
         jmp go_on2
-        
             print_digit_in_al2:
                 add al, '0'
 
@@ -171,16 +195,14 @@ segment code use32 class=code
                 call [fprintf]
                 add esp, 4*2
                 pop eax    
-                
         go_on2:
         mov al, bh
         mov ah, 0
         div byte [one]
-         ;get ones digit of minimum. if existent, print to file
+        ;get ones digit of minimum. if existent, print to file
         cmp al, 0
         jne print_digit_in_al3
         jmp go_on2
-            
             print_digit_in_al3:
                 add al, '0'
                 mov ah, 0
@@ -189,8 +211,7 @@ segment code use32 class=code
                 push dword [file_descriptor]
                 call [fprintf]
                 add esp, 4*2
-                
-        ; close file append mode
+                 ; close file append mode
         push dword [file_descriptor]
         call [fclose]
         add esp, 4
